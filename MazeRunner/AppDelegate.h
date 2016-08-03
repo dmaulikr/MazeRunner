373fd9d227507b8d87d25517d759f36889f7997c
willemlundy <willemlundy@gmail.com>
2016-08-03 14:03:06 -0700

    Initial Commit

---
+++ MazeRunner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MazeRunner
+//
+//  Created by William Lundy on 8/3/16.
+//  Copyright © 2016 William Lundy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MazeRunner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MazeRunner
+//
+//  Created by William Lundy on 8/3/16.
+//  Copyright © 2016 William Lundy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
