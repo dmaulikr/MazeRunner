66d7a790db7a5486b7ddc2b9e99b6f291e1b452c
willemlundy <willemlundy@gmail.com>
2016-08-07 14:19:52 -0700

    Created image types

--- MazeRunner/ViewController.m
+++ MazeRunner/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+
+
+@property (nonatomic, strong) NSString *imageName;
+@property (nonatomic, strong) NSDictionary *imageViewTranslationDictionary;
+
 // Score Labels
 
 
@@ -17,6 +22,13 @@
 // Grid Labels
 
 // 7th row down (Bottom row)
+@property (weak, nonatomic) IBOutlet UIImageView *oneOne;
+@property (weak, nonatomic) IBOutlet UIImageView *twoOne;
+@property (weak, nonatomic) IBOutlet UIImageView *threeOne;
+@property (weak, nonatomic) IBOutlet UIImageView *fourOne;
+@property (weak, nonatomic) IBOutlet UIImageView *fiveOne;
+@property (weak, nonatomic) IBOutlet UIImageView *sixOne;
+@property (weak, nonatomic) IBOutlet UIImageView *sevenOne;
 
 
 // 6th row down (Second from the bottom row)
@@ -41,16 +53,88 @@
 
 @end
 
+typedef NS_ENUM(NSInteger, ImageType)
+{
+    ImageTypeGrayDot = 1,
+    ImageTypePrize,
+    ImageTypeRedTeam,
+    ImageTypeRedFace,
+    ImageTypeRedBeenThere,
+    ImageTypeBlueTeam,
+    ImageTypeBlueFace,
+    ImageTypeBlueBeenThere
+};
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Build the location translation dictionary
+    self.imageViewTranslationDictionary = [[NSMutableDictionary alloc] initWithDictionary:@{ @11 : @"oneOne",
+                                                                                             @21 : @"twoOne",
+                                                                                             @31 : @"threeOne",
+                                                                                             @41 : @"fourOne",
+                                                                                             @51 : @"fiveOne",
+                                                                                             @61 : @"sixOne",
+                                                                                             @71 : @"sevenOne"}];
+
+    
+    [self changeLocation:@61 toImage:ImageTypeBlueFace];
+    [self changeLocation:@41 toImage:ImageTypePrize];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+- (void)changeLocationImage:(UIImageView *)location toImage:(NSString *)newImage {
+    
+    location.image = [UIImage imageNamed:newImage];
+}
+                             
+- (void)changeLocation:(NSNumber *)locationNumber toImage:(enum ImageType)newImage {
+    
+    UIImageView *locationImageView = [self valueForKey:[self.imageViewTranslationDictionary objectForKey:locationNumber]];
+    
+    switch(newImage) {
+        case ImageTypeGrayDot:
+            locationImageView.image = [UIImage imageNamed:@"GrayDot100"];
+            break;
+        case ImageTypePrize:
+            locationImageView.image = [UIImage imageNamed:@"Prize100"];
+            break;
+        case ImageTypeRedTeam:
+            locationImageView.image = [UIImage imageNamed:@"RedTeam100"];
+            break;
+        case ImageTypeRedFace:
+            locationImageView.image = [UIImage imageNamed:@"RedFace100"];
+            break;
+        case ImageTypeRedBeenThere:
+            locationImageView.image = [UIImage imageNamed:@"RedBeenThere100"];
+            break;
+        case ImageTypeBlueTeam:
+            locationImageView.image = [UIImage imageNamed:@"BlueTeam100"];
+            break;
+        case ImageTypeBlueFace:
+            locationImageView.image = [UIImage imageNamed:@"BlueFace100"];
+            break;
+        case ImageTypeBlueBeenThere:
+            locationImageView.image = [UIImage imageNamed:@"BlueBeenThere100"];
+            break;
+        default:
+            locationImageView.image = [UIImage imageNamed:@"GrayDot100"];
+            NSLog(@"%@", [NSString stringWithFormat:@"Unrecognized ImageType"]);
+            break;
+            
+    }
+    
+}
+                             
+                             
 
 @end
